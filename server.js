ec88725a125c9b8e09156e48728c0afbb6e72a5a
lmsand <lmsand99@icloud.com>
2024-03-15 20:51:22 -0500

    Arguments

--- server.js
+++ server.js
@@ -6,20 +6,34 @@ var { ruruHTML } = require("ruru/server")
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
+    hello(name: String!): String
     age: String
+    weight: Float
+    isOver18: Boolean
+
+    hobbies: [String!]!
   }
 `)
 
 // The rootValue provides a resolver function for each API endpoint
 var rootValue = {
-  hello: () => {
+  // hello: (args) => {
+  //   // fetch from db
+  //   // process
+  //   return "Hello, " + args.name
+  // },
+  hello: ({name}) => {
     // fetch from db
     // process
-    return "Hello world 123!"
+    return "Hello, " + name
   },
   age: () => {
     return "24"
+  },
+  weight: 77.7,
+  isOver18: true,
+  hobbies: () => {
+    return ["Carting", "F1", "Simulator"]
   }
 }
 
