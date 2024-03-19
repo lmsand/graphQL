502a7e3c69740a642be8d94909945a380b192a19
lmsand <lmsand99@icloud.com>
2024-03-19 10:18:56 -0500

    GraphQL Yoga

--- server.js
+++ server.js
@@ -1,101 +1,19 @@
-var { graphql, buildSchema, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql")
-var express = require('express')
-var { createHandler } = require("graphql-http/lib/use/express")
-var { ruruHTML } = require("ruru/server")
 
-// Construct a schema, using GraphQL schema language
-// var schema = buildSchema(`
-//   type Query {
-//     hello(name: String!): String
-//     age: String
-//     weight: Float
-//     isOver18: Boolean
+import express from 'express'
+import { ruruHTML } from "ruru/server"
 
-//     hobbies: [String!]!
+import { createYoga } from 'graphql-yoga'
+import {schema} from './src/graphql/index.js'
 
-//     user: User
-//   }
-
-//   type User {
-//     id: Int
-//     name: String
-//   }
-// `)
-
-const User = new GraphQLObjectType({
-  name: 'User',
-  fields: {
-    id: {
-      type: GraphQLInt
-    },
-    name: {
-      type: GraphQLString,
-      resolve: (obj) => {
-        return obj.name.trim().toLowerCase()
-      }
-    }
-  }
+const yoga = createYoga({
+  schema: schema
 })
 
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve: () => {
-          return 'Hello World'
-        },
-      },
-
-      user: {
-        type: User,
-        resolve: () => {
-          return {
-            id: 1,
-            name: 'lexi'
-          }
-        }
-      }
-    }
-  })
-})
-
-// The rootValue provides a resolver function for each API endpoint
-// var rootValue = {
-//   // hello: (args) => {
-//   //   // fetch from db
-//   //   // process
-//   //   return "Hello, " + args.name
-//   // },
-//   hello: ({name}) => {
-//     // fetch from db
-//     // process
-//     return "Hello, " + name
-//   },
-//   age: () => {
-//     return "24"
-//   },
-//   weight: 77.7,
-//   isOver18: true,
-//   hobbies: () => {
-//     return ["Carting", "F1", "Simulator"]
-//   },
-//   user: () => {
-//     return {
-//       id: 1,
-//       name: 'lexi'
-
-//     }
-//   }
-// }
-
-// Run the GraphQL query '{ hello }' and print out the response
 
 const app = express()
 
-app.all('/graphql', createHandler({ schema }))
+app.all('/graphql', yoga)
 
 // Serve the GraphiQL IDE.
 app.get("/", (_req, res) => {
---
+++ src/graphql/models/user.js
@@ -0,0 +1,26 @@
+export const typeDef = /* GraphQL */ `
+  type Query {
+    user: User
+  }
+
+  type User {
+    id: Int
+    name: String
+  }
+  `;
+
+export const resolvers = {
+  Query: {
+    user: () => {
+      return {
+        id: 1,
+        name: 'lexi'
+      }
+    }
+  },
+  User: {
+    name: (obj) => {
+      return obj.name.toUpperCase()
+    }
+  }
+}
